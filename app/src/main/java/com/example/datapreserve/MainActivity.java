8038a8c86b8e843a142b59e6fead0c3d82f0ba37
Kumoichi <minimi_ken1@yahoo.co.jp>
2022-10-21 09:44:29 -0700

    store title, authors, pages

---
+++ app/src/main/java/com/example/datapreserve/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.datapreserve;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+    }
+
+    public void adding(View view) {
+        Intent intent = new Intent(MainActivity.this, AddActivity.class);
+        startActivity(intent);
+
+    }
+}
